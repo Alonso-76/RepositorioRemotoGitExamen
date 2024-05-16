303e181a753c5711345ec6ce4f952784daef52c5
ACM1 <zahimon@hotmail.com>
2024-05-16 18:45:55 +0200

    Examen

--- ProyectoGitExamen/src/paqueteGitExamen/Main2.java
+++ ProyectoGitExamen/src/paqueteGitExamen/Main2.java
@@ -1,5 +1,8 @@
 package paqueteGitExamen;
 
 public class Main2 {
-
+	public static void main(String[] args) {
+		System.out.print("Este es el primer mensaje");
+		System.out.print("Este es el segundo mensaje");
+	}
 }
